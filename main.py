cdc2c4e24eea690909cd378acd98c2837720ec45
viktoriussuwandi <viktorius.tori@gmail.com>
2023-05-16 08:38:30 +0000

    create function to generate data

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from morse import Morse
 
-morse = Morse()
-data = morse.df
+def get_data() :
+  from morse import Morse
+  morse  = Morse()
+  data   = morse.df.to_dict()
+  return data
 
-inputs = input('Insert some words : ')
+def get_result() :
+  data = get_data()
+  chars  = [ val for (key,val) in data['char'].items() ]
+  morses = [ val for (key,val) in data['code'].items() ]
 
-for a in inputs :
-  if a.upper() in data.char : 
-    print(f'{a}')
+  # inputs = input('Insert some words : ')
+  inputs  = ['c', 'h', 'a', 'r', 'l', 'l', 'i', 'e']
+  
+  index_results = [
+    chars.index(i.upper()) for i in inputs if 
+    i.upper() in [v for v in chars]
+  ]
+  
+  results = [ morses[i] for i in index_results ]
+
+  for i in range( len(inputs)-1 ) :
+    input = inputs[i].upper()
+    if input not in [ c for c in chars] : print(f'{ inputs[i] } : ')
+    else :
+      index_result = 
+      print(f'{ inputs[i] } : { results[ chars.index(input) ] }')
+
+get_result()
